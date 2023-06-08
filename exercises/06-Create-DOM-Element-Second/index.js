c0a906940675228a95a47c1034eefa7acf9749ed
Mostafa Mazhar <moustafammazhar@gmail.com>
2023-06-08 16:33:38 -0400

    06 append img to the body

--- exercises/06-Create-DOM-Element-Second/index.js
+++ exercises/06-Create-DOM-Element-Second/index.js
@@ -1,4 +1,6 @@
 document.body.innerHTML = "<h1>Hello World</h1>"; 
 //you code here
-
+const img = document.createElement('img');
+img.src = "https://via.placeholder.com/350x150";
+document.body.appendChild(img);
 
