5254492927f2a0d0a0c44d1fd8cbcd691e56843a
Mostafa Mazhar <moustafammazhar@gmail.com>
2023-06-08 17:57:31 -0400

    10 -Added fouth li to the ul

--- exercises/10-Add-li-on-Click/index.js
+++ exercises/10-Add-li-on-Click/index.js
@@ -1,5 +1,8 @@
 let button = document.querySelector("#superDuperButton");
+let myList = document.querySelector("#myList");
 button.addEventListener("click", function() {
 	//your code here
-
+   let newLi = document.createElement("li");
+	 newLi.innerHTML = "FourthElemnt";
+	 myList.appendChild(newLi);
 });
