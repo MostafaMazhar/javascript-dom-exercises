d1e81b94f8383cd3d4fb50b1a18ba6eb85c96e16
Mostafa Mazhar <moustafammazhar@gmail.com>
2023-06-08 18:33:21 -0400

    12- loop through arr & change events

--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -1,2 +1,14 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 // your code here
+let countryNames = document.querySelector("#mySelect");
+
+for(let i = 0; i < countries.length; i++){
+  let optionElement = document.createElement("option");
+  optionElement.value = countries[i];
+  optionElement.innerHTML = countries[i];
+  countryNames.appendChild(optionElement);
+}
+countryNames.addEventListener("change", function() {
+   let selectedCountry = countryNames.value;
+   alert(selectedCountry);
+}) 
